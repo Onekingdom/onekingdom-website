72532c589ba23a2ac0f723a7dcf3b75ab2e09285
jochemwhite <j.vanderwit@amrio.nl>
2024-02-13 10:36:17 +0100

    Add new constants and types

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -99,3 +99,26 @@ export const spotifyRedirectURL = baseURl + "/api/spotify/callback"
 
 
 export const SpotifyScopes = []
+
+export type EditorBtns =
+  | 'text'
+  | 'container'
+  | 'section'
+  | 'contactForm'
+  | 'paymentForm'
+  | 'link'
+  | '2Col'
+  | 'video'
+  | '__body'
+  | 'image'
+  | null
+  | '3Col'
+
+
+  export const defaultStyles: React.CSSProperties = {
+    backgroundPosition: 'center',
+    objectFit: 'cover',
+    backgroundRepeat: 'no-repeat',
+    textAlign: 'left',
+    opacity: '100%',
+  }
